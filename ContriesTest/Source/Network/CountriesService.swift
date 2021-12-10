afee49740c500f320b0e768a84c46e060354d709
Kim Cortez <kimislecortez@gmail.com>
2021-12-10 18:11:59 +0800

    Initial setup for network

---
+++ ContriesTest/Source/Network/CountriesService.swift
@@ -0,0 +1,44 @@
+//
+//  CountriesService.swift
+//  CountriesTest
+//
+//  Created by Kim Isle Cortez on 12/10/21.
+//
+
+import Foundation
+
+// MARK: - Countries
+enum CountriesService: APIService {
+	
+	case searchCountry(keyword: String)
+	
+	var path: String {
+		switch self {
+		case .searchCountry(let keyword):
+			return "/name/\(keyword)"
+		}
+	}
+	
+	var method: String {
+		switch self {
+		default:
+			return "get"
+		}
+	}
+	
+	var parameters: [String : Any]? {
+		switch self {
+		default:
+			return nil
+		}
+	}
+	
+	var header: [String : String]? {
+		switch self {
+		default:
+			return nil
+		}
+	}
+}
+
+// MARK: - Add more services
